6cb55afd5ccedb426c2ae82d6407b520a49a7d2c
masatomo3162-lgtm <masatomo3162@gmail.com>
2026-07-04 11:24:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='barber-unified-v1.0.1';
+const CACHE='barber-unified-v1.0.2';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
